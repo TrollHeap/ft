074397fc36f2a45897abdfb83b382b825f6837a9
Binary-grunt <CodeGrunt@proton.me>
2025-01-27 02:27:32 +0100

    feat: Add ft_memset function and rearrange functions in libft.h

--- libft/libft.h
+++ libft/libft.h
@@ -24,11 +24,11 @@ int ft_isascii(int c);
 int ft_isprint(int c);
 int ft_toupper(int c);
 int ft_tolower(int c);
+int ft_strncmp(const char *s1, const char *s2, size_t n);
 
+void *ft_memset(void *b, int c, size_t len);
 char *ft_strchr(const char *s, int c);
 char *ft_strrchr(const char *s, int c);
-
-int ft_strncmp(const char *s1, const char *s2, size_t n);
 
 size_t ft_strlen(const char *str);
 
